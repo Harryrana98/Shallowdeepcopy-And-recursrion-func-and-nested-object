978c829c9feb3ea5766cf044083cde9ee0704748
Harryrana98 <ranaharendra77@gmail.com>
2025-04-11 11:14:33 +0530

    ...

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const person={
         city:"Bharatpur",
         state:"Rajasthan",
     },
+    Hobbies:{
+            game:{
+            indoor_game:{
+                boardGame:["LUDO","CHESS",]
+            },
+            outdoor_game:["CRICKET","FOOTBALL","HOCKEY"]
+
+            
+            }
+    },
     skills:{
         frontent:["HTML","CSS","JS"],
         backend:["NODE","PHP"]
@@ -27,25 +37,45 @@ const person={
 //     }
 //   }
 
-  function flattenObject(obj, nesting=false,key=null){
-    if(Array.isArray(obj[key])){
+//   function flattenObject(obj, nesting=false,key=null){
+//     if(Array.isArray(obj[key])){
       
-    }
-else{
-        for(let i in obj){
-            if(typeof obj[i]==="object"){
-                flattenObject(obj[i],true,i)
-            }else {
+//     }
+// else{
+//         for(let i in obj){
+//             if(typeof obj[i]==="object"){
+//                 flattenObject(obj[i],true,i)
+//             }else {
 
-                if(nesting){
-                    document.writeln(`${key}_${i}:${obj[i]}<br/>`)
-                }
-               else{
-                document.writeln(`${i}:${obj[i]}<br/>`)
-               }
-            }
+//                 if(nesting){
+//                     document.writeln(`${key}_${i}:${obj[i]}<br/>`)
+//                 }
+//                else{
+//                 document.writeln(`${i}:${obj[i]}<br/>`)
+//                }
+//             }
+//         }
+//     }
+//   }
+
+//   flattenObject(person)
+
+
+function flattenObject(obj, prefix = '') {
+    for (let key in obj) {
+        const value = obj[key];
+        const newKey = prefix ? `${prefix}_${key}` : key;
+
+        if (typeof value === 'object' && !Array.isArray(value)) {
+            // Recursive call for nested objects
+            flattenObject(value, newKey);
+        } else if (Array.isArray(value)) {
+            // Join array values into a string
+            document.writeln(`${newKey}:${value.join(',')}<br/>`);
+        } else {
+            // Primitive value
+            document.writeln(`${newKey}:${value}<br/>`);
         }
     }
-  }
-
-  flattenObject(person)
+}
+flattenObject(person)
